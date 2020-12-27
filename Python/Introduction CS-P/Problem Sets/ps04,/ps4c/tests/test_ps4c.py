932db55ae0ab4fa676868ef9b76118a5d1203126
Alon Parag <allon.parag@gmail.com>
2020-12-27 18:43:39 +0200

    Homework update 27/12/2020

--- Python/Introduction CS-P/Problem Sets/ps04,/ps4c/tests/test_ps4c.py
+++ Python/Introduction CS-P/Problem Sets/ps04,/ps4c/tests/test_ps4c.py
@@ -5,81 +5,58 @@ from ps4a import get_permutations
 from ps4c import *
 
 @pytest.mark.parametrize('input_value, return_value, expected',[
-    (SubMessage('Converting Vegetarians'),'Converting Vegetarians', True),
+    (SubMessage('Converting juice'),'Converting juice', True),
     (SubMessage('Into the system!'),'Into the system!', True),
-    (SubMessage('Converting Vegetarians'),'', False),
-    (SubMessage('Into the system!'),'Into the system!', False)
+    (SubMessage('Converting juice'),'', False),
+    (SubMessage('Into the system!'),'Intro the system!', False)
     ])
-@pytest.mark.skip
+
 def test_get_message_text(input_value, return_value, expected):
     if expected:
-        assert input_value == return_value
+        assert input_value.get_message_text() == return_value
     else:
-        assert input_value != return_value
+        assert input_value.get_message_text() != return_value
 
 @pytest.mark.parametrize('input_value, return_value, expected',[
-    (SubMessage('Converting Vegetarians'),['Converting',' Vegetarians'], True),
+    (SubMessage('Converting juice'),['Converting','juice'], True),
     (SubMessage('Into the system!'),['Into','the','system!'], True),
-    (SubMessage('Converting Vegetarians'),'', False),
+    (SubMessage('Converting juice'),'', False),
     (SubMessage('Into the system!'),'', False)
     ])
 
-@pytest.mark.skip
+
 def test_get_valid_words(input_value, return_value, expected):
     if expected:
-        assert input_value == return_value
+        assert input_value.get_valid_words() == return_value
     else:
-        assert input_value != return_value
+        assert input_value.get_valid_words() != return_value
 
-@pytest.mark.parametrize('input_value, return_value, expected',[
-    (SubMessage('Converting Vegetarians'),{
-    'A':"U",
-    'E':"A",
-    'I':"E",
-    'O':"I",
-    'U':"O",
-    'a':"u",
-    'e':"a",
-    'i':"e",
-    'o':"i",
-    'u':"o"
-}, True),
-    (SubMessage('Into the system!'),{
-    'A':"E",
-    'E':"I",
-    'I':"O",
-    'O':"U",
-    'U':"A",
-    'a':"e",
-    'e':"i",
-    'i':"o",
-    'o':"u",
-    'u':"a"
-}, True),
-    (SubMessage('Converting Vegetarians'),'', False),
-    (SubMessage('Into the system!'),'', False)
+@pytest.mark.parametrize('input_value,permutation, return_value, expected',[
+    (SubMessage('Converting juice'),'uaeio',{'A':'U', 'B':'B', 'C':'C', 'D':'D', 'E':'A', 'F':'F', 'G':'G', 'H':'H', 'I':'E', 'J':'J', 'K':'K', 'L':'L', 'M':'M', 'N':'N', 'O':'I', 'P':'P', 'Q':'Q', 'R':'R', 'S':'S', 'T':'T', 'U':'O', 'V':'V', 'W':'W', 'X':'X', 'Y':'Y', 'Z':'Z','a':'u', 'b':'b', 'c':'c', 'd':'d', 'e':'a', 'f':'f', 'g':'g', 'h':'h', 'i':'e', 'j':'j', 'k':'k', 'l':'l', 'm':'m', 'n':'n', 'o':'i', 'p':'p', 'q':'q', 'r':'r', 's':'s', 't':'t', 'u':'o', 'v':'v', 'w':'w', 'x':'x', 'y':'y', 'z':'z'}, True),
+    (SubMessage('Into the system!'),'eioua',{'A':'E', 'B':'B', 'C':'C', 'D':'D', 'E':'I', 'F':'F', 'G':'G', 'H':'H', 'I':'O', 'J':'J', 'K':'K', 'L':'L', 'M':'M', 'N':'N', 'O':'U', 'P':'P', 'Q':'Q', 'R':'R', 'S':'S', 'T':'T', 'U':'A', 'V':'V', 'W':'W', 'X':'X', 'Y':'Y', 'Z':'Z','a':'e', 'b':'b', 'c':'c', 'd':'d', 'e':'i', 'f':'f', 'g':'g', 'h':'h', 'i':'o', 'j':'j', 'k':'k', 'l':'l', 'm':'m', 'n':'n', 'o':'u', 'p':'p', 'q':'q', 'r':'r', 's':'s', 't':'t', 'u':'a', 'v':'v', 'w':'w', 'x':'x', 'y':'y', 'z':'z'}, True),
+    (SubMessage('Converting juice'),'ueiao','', False),
+    (SubMessage('Into the system!'),'ueiao','', False)
     ])
 
-@pytest.mark.skip
-def test_build_transpose_dict(input_value, return_value, expected):
+def test_build_transpose_dict(input_value,permutation, return_value, expected):
     if expected:
-        assert input_value == return_value
+        assert input_value.build_transpose_dict(permutation) == return_value
     else:
-        assert input_value != return_value
+        assert input_value.build_transpose_dict(permutation) != return_value
 
-@pytest.mark.parametrize('input_value, return_value, expected',[
-    (SubMessage('Converting Vegetarians'),'', True),
-    (SubMessage('Into the system!'),'', True),
-    (SubMessage('Converting Vegetarians'),'', False),
-    (SubMessage('Into the system!'),'', False)
+@pytest.mark.parametrize('input_value, permutation, return_value, expected',[
+    (SubMessage('Converting juice'),'eauio','Cinvartung jouca', True),
+    (SubMessage('Into the system!'),'eaiuo','Intu tha systam!', True),
+    (SubMessage('Converting juice'),'eauio','', False),
+    (SubMessage('Into the system!'),'eaiuo','', False)
     ])
 
-@pytest.mark.skip
-def test_apply_transpose(input_value, return_value, expected):
+
+def test_apply_transpose(input_value, permutation, return_value, expected):
     if expected:
-        assert input_value == return_value
+        assert input_value.apply_transpose(input_value.build_transpose_dict(permutation)) == return_value
     else:
-        assert input_value != return_value
+        assert input_value.apply_transpose(input_value.build_transpose_dict(permutation)) != return_value
 
 # @pytest.mark.parametrize('',[])
 @pytest.mark.skip
